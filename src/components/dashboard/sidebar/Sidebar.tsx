cf6a1d8fd3844a70b880a131d13f8265d94031c8
biniruu <biniruu@gmail.com>
2024-01-24 19:40:46 +0900

    refactor: isolate menuItems from Sidebar

--- src/components/dashboard/sidebar/Sidebar.tsx
+++ src/components/dashboard/sidebar/Sidebar.tsx
@@ -1,81 +1,9 @@
 import Image from 'next/image'
-import {
-  MdAnalytics,
-  MdAttachMoney,
-  MdDashboard,
-  MdHelpCenter,
-  MdOutlineSettings,
-  MdPeople,
-  MdShoppingBag,
-  MdSupervisedUserCircle,
-  MdWork,
-} from 'react-icons/md'
 
 import MenuLink from './menuLink/MenuLink'
 import styles from './sidebar.module.css'
 
-const menuItems = [
-  {
-    title: 'Pages',
-    list: [
-      {
-        title: 'Dashboard',
-        path: '/dashboard',
-        icon: <MdDashboard />,
-      },
-      {
-        title: 'Users',
-        path: '/dashboard/users',
-        icon: <MdSupervisedUserCircle />,
-      },
-      {
-        title: 'Products',
-        path: '/dashboard/products',
-        icon: <MdShoppingBag />,
-      },
-      {
-        title: 'Transactions',
-        path: '/dashboard/transactions',
-        icon: <MdAttachMoney />,
-      },
-    ],
-  },
-  {
-    title: 'Analytics',
-    list: [
-      {
-        title: 'Revenue',
-        path: '/dashboard/revenue',
-        icon: <MdWork />,
-      },
-      {
-        title: 'Reports',
-        path: '/dashboard/reports',
-        icon: <MdAnalytics />,
-      },
-      {
-        title: 'Teams',
-        path: '/dashboard/teams',
-        icon: <MdPeople />,
-      },
-    ],
-  },
-  {
-    title: 'User',
-    list: [
-      {
-        title: 'Settings',
-        path: '/dashboard/settings',
-        icon: <MdOutlineSettings />,
-      },
-      {
-        title: 'Help',
-        path: '/dashboard/help',
-        icon: <MdHelpCenter />,
-      },
-    ],
-  },
-]
+import menuItems from 'data/menuItems'
 
 function Sidebar() {
   return (
---
+++ src/data/menuItems.tsx
@@ -0,0 +1,76 @@
+import {
+  MdAnalytics,
+  MdAttachMoney,
+  MdDashboard,
+  MdHelpCenter,
+  MdOutlineSettings,
+  MdPeople,
+  MdShoppingBag,
+  MdSupervisedUserCircle,
+  MdWork,
+} from 'react-icons/md'
+
+const menuItems = [
+  {
+    title: 'Pages',
+    list: [
+      {
+        title: 'Dashboard',
+        path: '/dashboard',
+        icon: <MdDashboard />,
+      },
+      {
+        title: 'Users',
+        path: '/dashboard/users',
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: 'Products',
+        path: '/dashboard/products',
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: 'Transactions',
+        path: '/dashboard/transactions',
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: 'Analytics',
+    list: [
+      {
+        title: 'Revenue',
+        path: '/dashboard/revenue',
+        icon: <MdWork />,
+      },
+      {
+        title: 'Reports',
+        path: '/dashboard/reports',
+        icon: <MdAnalytics />,
+      },
+      {
+        title: 'Teams',
+        path: '/dashboard/teams',
+        icon: <MdPeople />,
+      },
+    ],
+  },
+  {
+    title: 'User',
+    list: [
+      {
+        title: 'Settings',
+        path: '/dashboard/settings',
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: 'Help',
+        path: '/dashboard/help',
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+]
+
+export default menuItems

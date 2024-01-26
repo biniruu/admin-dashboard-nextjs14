f0ddee76c7610d514507b23919786476f032ebb6
biniruu <biniruu@gmail.com>
2024-01-26 20:00:19 +0900

    feat: build layout of Card

--- src/app/dashboard/card/Card.tsx
+++ src/app/dashboard/card/Card.tsx
@@ -1,5 +1,21 @@
+import { MdSupervisedUserCircle } from 'react-icons/md'
+
+import styles from './card.module.css'
+
 function Card() {
-  return <div>Card</div>
+  return (
+    <div>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}>10.273</span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>12%</span>
+          more than
+        </span>
+      </div>
+    </div>
+  )
 }
 
 export default Card

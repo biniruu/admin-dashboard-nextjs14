f0a0c02c1c4e48210dedcc39741acebfda5301b9
biniruu <biniruu@gmail.com>
2024-01-26 19:23:37 +0900

    feat: build layout of Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,26 @@
-function DashboardPage() {
-  return <div>DashboardPage</div>
+import Card from './card/Card'
+import Chart from './chart/Chart'
+import styles from './dashboard.module.css'
+import Rightbar from './rightbar/Rightbar'
+import Transactions from './transactions/Transactions'
+
+function Dashboard() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.card}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  )
 }
 
-export default DashboardPage
+export default Dashboard

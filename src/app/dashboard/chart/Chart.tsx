b0c19a4f374e6d21e1875c38101bcac57dba1fcc
biniruu <biniruu@gmail.com>
2024-01-27 19:27:11 +0900

    feat: update chart in Chart
    remove CartesianGrid
    add contentStyle property to Tooltip

--- src/app/dashboard/chart/Chart.tsx
+++ src/app/dashboard/chart/Chart.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis } from 'recharts'
+import { Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis } from 'recharts'
 
 import styles from './chart.module.css'
 
@@ -12,10 +12,9 @@ function Chart() {
       <h2>weekly recap</h2>
       <ResponsiveContainer width="100%" height="100%">
         <LineChart width={500} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-          <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <XAxis />
-          <Tooltip />
+          <Tooltip contentStyle={{ background: '#151c2c', border: 'none' }} />
           <Legend />
           <Line type="monotone" dataKey="visit" stroke="#8884d8" strokeDasharray="5 5" />
           <Line type="monotone" dataKey="click" stroke="#82ca9d" strokeDasharray="3 4 5 2" />

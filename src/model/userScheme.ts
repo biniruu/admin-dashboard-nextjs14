7c53d71ff42e615ada8558ae9a449bcd33b5e865
biniruu <biniruu@gmail.com>
2024-01-29 16:33:01 +0900

    chore: create userScheme.ts

---
+++ src/model/userScheme.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    phone: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+)
+
+export const User = mongoose.models.User || mongoose.model('User', userSchema)

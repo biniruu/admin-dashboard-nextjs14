ff84bb2368e50ef71d9eff43b0973698e9cca265
biniruu <biniruu@gmail.com>
2024-01-29 23:32:43 +0900

    chore: update tailwind configuration
    Added properties of backgroundColor, colors.
    Added borderRadius option

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,16 +11,23 @@ const config: Config = {
       zIndex: {},
       fontSize: {},
       colors: {
-        green: '#11ac3a',
-        red: '#e33920',
-        yellow: '#dfb230',
+        lime: 'lime',
+        red: 'red',
+        text: 'white',
+        'text-soft': '#b7bac1',
       },
       backgroundColor: ({ theme }) => ({
         ...theme('colors'),
+        bg: '#151c2c',
+        'bg-soft': '#182237',
+        hover: '#2e374a',
       }),
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      borderRadius: {
+        DEFAULT: '0.625rem',
       },
     },
   },

3a182dad398e2eddebc3b56858d03fbe82837692
biniruu <biniruu@gmail.com>
2024-01-29 19:50:57 +0900

    feat: refactor Rightbar for simplicity
    Simplified the sturcture for improved readability

--- src/app/dashboard/rightbar/Rightbar.tsx
+++ src/app/dashboard/rightbar/Rightbar.tsx
@@ -1,45 +1,35 @@
 import Image from 'next/image'
-import { MdPlayCircleFilled, MdReadMore } from 'react-icons/md'
 
 import styles from './rightbar.module.css'
+
+import rightbarItems from 'data/rightbarItems'
 
 function Rightbar() {
   return (
     <div className={styles.container}>
-      <div className={styles.item}>
-        <div className={styles['bg-container']}>
-          <Image src="/astronaut.png" alt="" fill className={styles.bg} />
-        </div>
-        <div className={styles.text}>
-          <div className={styles.notification}>🔥 available now</div>
-          <h3 className={styles.title}>how to use the new version of the admin dashboard?</h3>
-          <div className={styles.subtitle}>takes 4 minutes to learn</div>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ex nemo doloribus temporibus quod excepturi,
-            eius magni inventore ratione expedita reprehenderit. Fugit tenetur corrupti laudantium minus. Commodi
-            quibusdam corrupti non!
-          </p>
-          <button className={styles.button}>
-            <MdPlayCircleFilled />
-            watch
-          </button>
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.text}>
-          <div className={styles.notification}>🚀 coming soon</div>
-          <h3 className={styles.title}>new server actions are available, partial pre-rendering is coming up!</h3>
-          <div className={styles.subtitle}>boost your productivity</div>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eius libero perspiciatis recusandae
-            possimus.
-          </p>
-          <button className={styles.button}>
-            <MdReadMore />
-            learn
-          </button>
-        </div>
-      </div>
+      {rightbarItems.map(item => {
+        const { notification, title, subtitle, desc, buttonIcon, button } = item
+
+        return (
+          <div className={styles.item} key={item.button}>
+            {item.button === 'watch' && (
+              <div className={styles['bg-container']}>
+                <Image src="/astronaut.png" alt="" fill className={styles.bg} />
+              </div>
+            )}
+            <div className={styles.text}>
+              <div className={styles.notification}>{notification}</div>
+              <h3 className={styles.title}>{title}</h3>
+              <div className={styles.subtitle}>{subtitle}</div>
+              <p className={styles.desc}>{desc}</p>
+              <button className={styles.button}>
+                {buttonIcon}
+                {button}
+              </button>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
---
+++ src/data/rightbarItems.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react'
+import { MdPlayCircleFilled, MdReadMore } from 'react-icons/md'
+
+interface RightbarItem {
+  notification: string
+  title: string
+  subtitle: string
+  desc: string
+  buttonIcon: ReactNode
+  button: string
+}
+
+const rightbarItems: RightbarItem[] = [
+  {
+    notification: '🔥 available now',
+    title: 'how to use the new version of the admin dashboard?',
+    subtitle: 'takes 4 minutes to learn',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ex nemo doloribus temporibus quod excepturi, eius magni inventore ratione expedita reprehenderit. Fugit tenetur corrupti laudantium minus. Commodi quibusdam corrupti non!',
+    buttonIcon: <MdPlayCircleFilled />,
+    button: 'watch',
+  },
+  {
+    notification: '🚀 coming soon',
+    title: 'new server actions are available, partial pre-rendering is coming up!',
+    subtitle: 'boost your productivity',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eius libero perspiciatis recusandae possimus.',
+    buttonIcon: <MdReadMore />,
+    button: 'learn',
+  },
+]
+
+export default rightbarItems

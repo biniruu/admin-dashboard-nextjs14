39eed9ce526ee13fa4eccd458268748edf33cde0
biniruu <biniruu@gmail.com>
2024-01-29 16:21:39 +0900

    feat: build users page

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,5 +1,55 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import styles from './users.module.css'
+
+import Pagination from 'components/pagination/Pagination'
+import Searchbar from 'components/searchbar/Searchbar'
+
 function UsersPage() {
-  return <div>UsersPage</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Searchbar placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles['add-button']}>add new</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>name</td>
+            <td>email</td>
+            <td>created at</td>
+            <td>role</td>
+            <td>status</td>
+            <td>action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image src="/noavatar.png" alt="" width={40} height={40} className={styles['user-image']} />
+                john doe
+              </div>
+            </td>
+            <td>john@gmail.com</td>
+            <td>13.01.2022</td>
+            <td>admin</td>
+            <td>active</td>
+            <td>
+              <Link href="/">
+                <button className={`${styles.button} ${styles.view}`}>view</button>
+              </Link>
+              <button className={`${styles.button} ${styles.delete}`}>delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  )
 }
 
 export default UsersPage

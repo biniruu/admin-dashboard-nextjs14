e8b44574afea8eddd62dec865854387fdd913ce4
biniruu <biniruu@gmail.com>
2024-01-29 23:28:59 +0900

    feat: convert css class into tailwind
    Converted css class name as postcss into tailwind

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,18 +1,20 @@
-import styles from './card.module.css'
-
 import { type CardData } from 'types'
 
 function Card({ icon, title, number, state, rate, detail }: CardData) {
   return (
-    <div className={styles.container}>
+    <div
+      className={['cursor-pointer', 'flex', 'w-full', 'bg-bg-soft', 'rounded', 'p-5', 'gap-5', 'hover:bg-hover'].join(
+        ' ',
+      )}
+    >
       {icon}
-      <div className={styles.texts}>
-        <span className={styles.title}>{title}</span>
-        <span className={styles.number}>{number}</span>
-        <span className={styles.detail}>
-          <span className={styles[state]}>{rate}%</span>
+      <div className={['flex', 'flex-col', 'gap-5'].join(' ')}>
+        <div>{title}</div>
+        <div className={['text-2xl', 'font-medium'].join(' ')}>{number}</div>
+        <div className={['text-sm', 'font-light'].join(' ')}>
+          <span className={[state === 'positive' ? 'text-lime' : 'text-red'].join(' ')}>{rate}%</span>
           {detail}
-        </span>
+        </div>
       </div>
     </div>
   )

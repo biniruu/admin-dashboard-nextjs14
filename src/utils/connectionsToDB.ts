c3cdf4c49ed704217b0e3f303b2be5bcba54f57e
biniruu <biniruu@gmail.com>
2024-02-01 02:10:20 +0900

    feat: implement connectToDB method

---
+++ src/utils/connectionsToDB.ts
@@ -0,0 +1,18 @@
+import { connect, type Error } from 'mongoose'
+
+let isConnected = true
+
+export const connectToDB = async () => {
+  if (!isConnected) {
+    return
+  }
+
+  try {
+    const db = await connect(process.env.MONGODB as string)
+    // if a value of readyState is 'disconnected', isConnected will become 'false'
+    isConnected = !!db.connections[0].readyState
+  } catch (error) {
+    const err = error as Error
+    throw err
+  }
+}

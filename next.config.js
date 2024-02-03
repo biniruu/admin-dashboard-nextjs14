184316b5ec0ee159308fcfb47e08074b370db55f
biniruu <biniruu@gmail.com>
2024-02-03 19:00:56 +0900

    chore: remove comments in configuration of next
    Remove unnecessary comments.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // compiler: {
-  //   styledComponents: true, // styled component 활성화
-  // },
-  // experimental: {
-  //   serverActions: true, // App Router에서 server actions 활성화
-  // },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.pexels.com',
       },
-    ], // 외부 이미지 uri
+    ],
   },
   reactStrictMode: true,
 }

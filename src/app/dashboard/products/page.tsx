26b07ee59b8d74bd691df134c0bfeb95377a755e
biniruu <biniruu@gmail.com>
2024-02-08 13:17:14 +0900

    feat: seperate table element from products page

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
-import styles from './products.module.css'
-
 import Pagination from 'components/pagination/Pagination'
+import ProductsTable from 'components/products-table/ProductsTable'
 import Searchbar from 'components/searchbar/Searchbar'
 import { type Product } from 'types'
 
@@ -17,8 +15,6 @@ function ProductsPage() {
   // TODO: convert into importing data from database
   const deleteProduct = () => {}
 
-  const tableHead: string[] = ['title', 'description', 'price', 'created at', 'stock', 'action']
-
   return (
     <div className="mt-5 rounded-default bg-bg-soft p-5">
       <div className="flex items-center justify-between">
@@ -29,52 +25,7 @@ function ProductsPage() {
           </button>
         </Link>
       </div>
-      <table className={`${styles.table} w-full`}>
-        <thead>
-          <tr>
-            {tableHead.map(item => (
-              <td key={item}>{item}</td>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {products.map(product => {
-            const { id, img, title, desc, price, createdAt, stock } = product
-
-            return (
-              <tr key={id}>
-                <td>
-                  <div className="flex items-center gap-default">
-                    <Image
-                      src={img || '/noproduct.jpg'}
-                      alt=""
-                      width={40}
-                      height={40}
-                      className="rounded-half object-cover"
-                    />
-                    {title}
-                  </div>
-                </td>
-                <td>{desc}</td>
-                <td>${price}</td>
-                <td>{createdAt?.toString().slice(4, 16)}</td>
-                <td>{stock}</td>
-                <td>
-                  <div className="flex gap-default">
-                    <Link href={`/dashboard/products/${id}`}>
-                      <button className={`${styles.button} bg-[teal]`}>View</button>
-                    </Link>
-                    <form action={deleteProduct}>
-                      <input type="hidden" name="id" value={id} />
-                      <button className={`${styles.button} bg-[crimson]`}>Delete</button>
-                    </form>
-                  </div>
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <ProductsTable products={products} deleteProduct={deleteProduct} />
       <Pagination count={count} />
     </div>
   )
---
+++ src/components/products-table/ProductsTable.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import styles from './productsTable.module.css'
+
+import { type Product } from 'types'
+
+const tableHead: string[] = ['title', 'description', 'price', 'created at', 'stock', 'action']
+
+interface Props {
+  products: Product[]
+  deleteProduct: () => void
+}
+
+function ProductsTable({ products, deleteProduct }: Props) {
+  return (
+    <table className={`${styles.table} w-full`}>
+      <thead>
+        <tr>
+          {tableHead.map(item => (
+            <td key={item}>{item}</td>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {products.map(product => {
+          const { id, img, title, desc, price, createdAt, stock } = product
+
+          return (
+            <tr key={id}>
+              <td>
+                <div className="flex items-center gap-default">
+                  <Image
+                    src={img || '/noproduct.jpg'}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className="rounded-half object-cover"
+                  />
+                  {title}
+                </div>
+              </td>
+              <td>{desc}</td>
+              <td>${price}</td>
+              <td>{createdAt?.toString().slice(4, 16)}</td>
+              <td>{stock}</td>
+              <td>
+                <div className="flex gap-default">
+                  <Link href={`/dashboard/products/${id}`}>
+                    <button className={`${styles.button} bg-[teal]`}>View</button>
+                  </Link>
+                  <form action={deleteProduct}>
+                    <input type="hidden" name="id" value={id} />
+                    <button className={`${styles.button} bg-[crimson]`}>Delete</button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+export default ProductsTable

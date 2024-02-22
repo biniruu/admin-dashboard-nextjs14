337ffd5efe8f02fc46f33ae8d03ce45584d6ada9
biniruu <biniruu@gmail.com>
2024-02-22 18:00:41 +0900

    chore: update comments in postcss.config.js and more
    Updated comments into English.
    Remove an 'autoprefixer' plugin.

--- postcss.config.js
+++ postcss.config.js
@@ -11,9 +11,6 @@
  * Postcss-preset-env features
  * {@link https://preset-env.cssdb.org/features/}
  *
- * Additional options in autoprefixer
- * {@link https://github.com/postcss/autoprefixer#options}
- *
  * Optimising Tailwind CSS for production
  * {@link https://tailwindcss.com/docs/optimizing-for-production}
  *
@@ -22,17 +19,15 @@
  */
 
 module.exports = {
+  syntax: 'postcss-syntax', //  automatically switch the required PostCSS syntax by file extension/source
   plugins: {
-    '@tailwindcss/nesting': {}, // 항상 tailwindcss 앞에 위치
+    '@tailwindcss/nesting': {}, // this plugin has to come before 'tailwindcss'
     'postcss-preset-env': {
-      autoprefixer: {
-        // grid: 'autoplace', // ie 10-11 대응 grid layout 속성 prefix
-      },
       features: {
-        'nesting-rules': false, // @tailwindcss/nesting 사용 시 false로 설정
+        'nesting-rules': false, // ensure this value as false when using @tailwindcss/nesting
       },
     },
-    tailwindcss: {}, // tailwindcss 사용 시 필요
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}), // production 환경에서 css minify 구현
+    tailwindcss: {}, // for using tailwindcss
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}), // invoking CSS minification in production environment
   },
 }

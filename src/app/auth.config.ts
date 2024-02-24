9c02ccce9ad10bc7cf4dde7c2fd54cdd8deca4c6
biniruu <biniruu@gmail.com>
2024-02-25 01:52:30 +0900

    chore: create auth.config.ts

---
+++ src/app/auth.config.ts
@@ -0,0 +1,21 @@
+import { type NextAuthConfig } from 'next-auth'
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
+      if (isOnDashboard) {
+        return isLoggedIn ? true : false
+      }
+      if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl))
+      }
+      return true
+    },
+  },
+  providers: [],
+} satisfies NextAuthConfig

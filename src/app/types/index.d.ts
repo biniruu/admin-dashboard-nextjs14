a75426a50a5042eece733153d18e25fecd75f7d3
biniruu <biniruu@gmail.com>
2024-02-25 03:27:13 +0900

    chore: add declares to index.d.ts

--- src/app/types/index.d.ts
+++ src/app/types/index.d.ts
@@ -35,3 +35,33 @@ export interface User {
   createdAt: Date
   updatedAt: Date
 }
+
+// auth.ts
+import 'next-auth'
+
+// TODO: declare for this project
+declare module 'next-auth' {
+  /**
+   * The shape of the user object returned in the OAuth providers' `profile` callback,
+   * or the second parameter of the `session` callback, when using a database.
+   */
+  interface User {}
+  /**
+   * The shape of the account object returned in the OAuth providers' `account` callback,
+   * Usually contains information about the provider being used, like OAuth tokens (`access_token`, etc).
+   */
+  interface Account {}
+
+  /**
+   * Returned by `useSession`, `auth`, contains information about the active session.
+   */
+  interface Session {}
+}
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    idToken?: string
+  }
+}

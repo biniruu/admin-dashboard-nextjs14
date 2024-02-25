00d8254335f2b591df1eb9bee6465241dedac88c
biniruu <biniruu@gmail.com>
2024-02-26 02:49:15 +0900

    style: improve readability on auth.config.ts

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,21 +1,25 @@
 import { type NextAuthConfig } from 'next-auth'
 
-export const authConfig = {
-  pages: {
-    signIn: '/login',
-  },
+const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
+
       if (isOnDashboard) {
         return isLoggedIn ? true : false
       }
       if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl))
       }
+
       return true
     },
   },
+  pages: {
+    signIn: '/login',
+  },
   providers: [],
 } satisfies NextAuthConfig
+
+export { authConfig }

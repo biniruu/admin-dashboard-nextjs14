6428b33dc8fb00703ffdda04ce1a7579bcae712b
biniruu <biniruu@gmail.com>
2024-02-26 22:44:46 +0900

    chore: set eslint rule
    import/consistent-type-specifier-style
    from 'prefer-inline' to 'top-level'

--- .eslintrc.js
+++ .eslintrc.js
@@ -218,13 +218,13 @@ module.exports = {
      * consistent-type-specifier-style : type-only import를 inline과 top-level 중 하나로만 사용하도록 강제
      * newline-after-import : import 다음에 한 줄 띄기
      * no-anonymous-default-export : 익명 default export 금지
-     * no-duplicates : 동일한 모듈에서 import를 여러 번 할 경우 모든 import를 inline 또는 top-level로 강제
+     * no-duplicates : enforce all imports to be inline or top-level when importing multiple times the same module.
      * no-unresolved : import한 파일/모듈이 unresolved 되는 일이 없도록 방지
      * order : import 자동 정렬
      * order > warnOnUnassignedImports는 항상 default값(false)으로 놔둘 것. true로 할 경우 import 정렬 관련 경고가 발생하는데, 이 문제는 import/order 또는 sort-import 설정만으로는 해결 불가
      * order > caseInsensitive의 값은 항상 default값(false)으로 놔둘 것. true로 했을 때 가끔 다른 import 정렬 관련 rule과 충돌 발생
      */
-    'import/consistent-type-specifier-style': ['error', 'prefer-inline'],
+    'import/consistent-type-specifier-style': 'warn',
     'import/newline-after-import': 'warn',
     'import/no-anonymous-default-export': [
       'warn',
@@ -233,12 +233,12 @@ module.exports = {
         allowObject: true,
       },
     ],
-    'import/no-duplicates': [
-      'error',
-      {
-        'prefer-inline': true,
-      },
-    ],
+    // 'import/no-duplicates': [
+    //   'error',
+    //   {
+    //     'prefer-inline': true,
+    //   },
+    // ], // to be enable if 'consistent-type-specifier-style' is 'prefer-inline'
     'import/no-unresolved': 'off',
     'import/order': [
       'warn',

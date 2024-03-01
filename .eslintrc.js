8bb83cf366460b98d26ce65bbbd5ca62768452b0
biniruu <biniruu@gmail.com>
2024-03-01 19:18:07 +0900

    chore: disable alphabetize option in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -221,6 +221,7 @@ module.exports = {
      * no-duplicates : enforce all imports to be inline or top-level when importing multiple times the same module.
      * no-unresolved : import한 파일/모듈이 unresolved 되는 일이 없도록 방지
      * order : import 자동 정렬
+     * order > alphabetize : Make sure it is always set as the default. If not, it can cause conflicts with prettier.
      * order > warnOnUnassignedImports는 항상 default값(false)으로 놔둘 것. true로 할 경우 import 정렬 관련 경고가 발생하는데, 이 문제는 import/order 또는 sort-import 설정만으로는 해결 불가
      * order > caseInsensitive의 값은 항상 default값(false)으로 놔둘 것. true로 했을 때 가끔 다른 import 정렬 관련 rule과 충돌 발생
      */
@@ -243,10 +244,10 @@ module.exports = {
     'import/order': [
       'warn',
       {
-        alphabetize: {
-          order: 'asc',
-          orderImportKind: 'asc',
-        },
+        // alphabetize: {
+        //   order: 'asc',
+        //   orderImportKind: 'asc',
+        // },
         'newlines-between': 'always',
       },
     ],

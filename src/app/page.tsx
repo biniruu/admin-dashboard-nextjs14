de17e9b278b6f821b396085da41bfde4d1363895
biniruu <biniruu@gmail.com>
2024-03-06 17:51:46 +0900

    feat: add button to main page
    navigate to dashboard on button click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     // TODO: add '/dashboard' route
-    <main className="flex items-center justify-center">
-      <div>Homepage</div>
+    <main className="flex h-screen items-center justify-center">
+      <Link href="/dashboard" className="rounded-full bg-[hotpink] px-5 py-2">
+        view dashboard🚀
+      </Link>
     </main>
   )
 }

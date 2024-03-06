5db4c197905ba355d899df60648081f9b9968697
biniruu <biniruu@gmail.com>
2024-03-06 18:39:10 +0900

    feat: update styles of login input forms

--- src/components/login-form/LoginForm.tsx
+++ src/components/login-form/LoginForm.tsx
@@ -12,12 +12,12 @@ function LoginForm() {
   return (
     <form
       action={formAction}
-      className="flex size-[31.25rem] flex-col items-center justify-center gap-[1.875rem] rounded-default bg-bg-soft p-[3.125rem]"
+      className="flex w-[31.25rem] flex-col items-center justify-center gap-[1.875rem] rounded-default bg-bg-soft p-[3.125rem]"
     >
-      <h1>login</h1>
+      <h1 className="hidden">login</h1>
       <input type="text" placeholder="username" name="username" className={styles.input} />
       <input type="password" placeholder="password" name="password" className={styles.input} />
-      <button className="w-full cursor-pointer border-none bg-teal-500 p-[1.875rem] text-text">login</button>
+      <button className="w-full cursor-pointer border-none bg-teal-500 p-6 capitalize text-text">login</button>
       {state && <div className="lowercase first-letter:capitalize">{state}</div>}
     </form>
   )

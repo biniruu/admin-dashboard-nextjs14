e8b78577e714ea8b1a8caac4c74cd0144e0ebd08
biniruu <biniruu@gmail.com>
2024-03-07 03:08:07 +0900

    feat: add clickEvent to RightSidebar

--- src/components/right-sidebar/RightSidebar.tsx
+++ src/components/right-sidebar/RightSidebar.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 
 import styles from './rightSidebar.module.css'
@@ -7,7 +9,7 @@ function RightSidebar() {
   return (
     <div className="fixed">
       {rightSidebarItems.map(item => {
-        const { notification, title, subtitle, desc, buttonIcon, button } = item
+        const { notification, title, subtitle, desc, buttonIcon, button, clickEvent } = item
 
         return (
           <div className={`relative mb-5 rounded-default px-6 py-5 ${styles.item}`} key={item.button}>
@@ -21,7 +23,10 @@ function RightSidebar() {
               <h3 className={`${styles.title} first-letter-capital`}>{title}</h3>
               <div className="first-letter-capital text-xs font-medium text-text-soft">{subtitle}</div>
               <p className="text-sm text-text-soft">{desc}</p>
-              <button className="flex w-max cursor-pointer items-center gap-default rounded-[0.3125rem] border-0 bg-[#5d57c9] p-default capitalize text-white">
+              <button
+                className="flex w-max cursor-pointer items-center gap-default rounded-[0.3125rem] border-0 bg-[#5d57c9] p-default capitalize text-white"
+                onClick={() => clickEvent('Noting to do.')}
+              >
                 {buttonIcon}
                 {button}
               </button>

4acc109eb2ac181fcbae1fa0fe507f067041570c
biniruu <biniruu@gmail.com>
2024-03-08 13:49:13 +0900

    feat: move passwordSecurity.ts from /utils to /lib

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -7,8 +7,8 @@ import { revalidatePath } from 'next/cache'
 import { isRedirectError } from 'next/dist/client/components/redirect'
 import { redirect } from 'next/navigation'
 
-import getHashedPassword from '../../utils/passwordSecurity'
 import { connectToDB } from './connectionsToDB'
+import getHashedPassword from './passwordSecurity'
 
 import { signIn } from 'auth'
 import { Product } from 'models/productScheme'

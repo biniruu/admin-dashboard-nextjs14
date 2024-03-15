ac18b247033c7e9aa6045b4208fe7b981d9e2970
biniruu <biniruu@gmail.com>
2024-03-16 01:46:35 +0900

    test: add unit test for getFilteredFields function
    the function is from helper.ts, the test code is in helper.test.ts

--- src/tests/utils/helpers.test.ts
+++ src/tests/utils/helpers.test.ts
@@ -1,7 +1,7 @@
 import type { Types } from 'mongoose'
 
 import type { FetchUser } from '@utils/helper'
-import { convertId, getParams } from '@utils/helper'
+import { convertId, getFilteredFields, getParams } from '@utils/helper'
 
 describe('helper functions', () => {
   test('should return an object with SearchKeywords and currentPage properties', () => {
@@ -19,27 +19,22 @@ describe('helper functions', () => {
     expect(result).toEqual(expected)
   })
 
-  test('should convert "id" property to "id" with a string type', () => {
-    const user = {
-      email: 'email@email.com',
-      phone: '0123456789',
-      address: 'my home',
-      password: '1234',
-      isAdmin: false,
-      isActive: false,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }
+  const user = {
+    username: 'John',
+    email: 'email@email.com',
+    phone: '0123456789',
+    address: '',
+    password: '1234',
+    isAdmin: false,
+    isActive: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
 
+  test('should convert "_id" property to "id" with a string type', () => {
     const data: FetchUser[] = [
       {
-        _id: 'John' as unknown as Types.ObjectId,
-        username: 'John',
-        ...user,
-      },
-      {
         _id: 'Smith' as unknown as Types.ObjectId,
-        username: 'Smith',
         ...user,
       },
     ]
@@ -47,17 +42,35 @@ describe('helper functions', () => {
     const result = convertId(data)
     const expected = [
       {
-        id: 'John',
-        username: 'John',
-        ...user,
-      },
-      {
         id: 'Smith',
-        username: 'Smith',
         ...user,
       },
     ]
 
     expect(result).toEqual(expected)
+  })
+
+  test('should return fields that have its values and "id" field from formData', () => {
+    const formData = new FormData()
+    formData.append('id', 'my-id')
+
+    for (const key in user) {
+      const field = key as keyof typeof user
+      const isDate = user[field] instanceof Date
+      const value = isDate ? (user[field] as Date).toISOString() : user[field].toString()
+      formData.append(key, value)
+    }
+
+    const result = getFilteredFields(formData)
+
+    expect(result).toHaveProperty('id', 'my-id')
+
+    const filteredFields = Object.fromEntries(formData)
+    // 'id' field is already checked
+    delete filteredFields.id
+    // the value of 'address' field is undefined (doesn't have a value)
+    delete filteredFields.address
+
+    expect(result).toHaveProperty('filteredFields', filteredFields)
   })
 })

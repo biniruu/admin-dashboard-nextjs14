c0c72785c2a2dc1ed32922b839af8b7c52e4bbf4
biniruu <biniruu@gmail.com>
2024-03-15 19:00:04 +0900

    test: add unit test for convertId function in helper.ts

--- src/tests/utils/helpers.test.ts
+++ src/tests/utils/helpers.test.ts
@@ -1,4 +1,7 @@
-import { getParams } from '@utils/helper'
+import type { Types } from 'mongoose'
+
+import type { FetchUser } from '@utils/helper'
+import { convertId, getParams } from '@utils/helper'
 
 describe('helper functions', () => {
   test('should return an object with SearchKeywords and currentPage properties', () => {
@@ -12,6 +15,48 @@ describe('helper functions', () => {
       searchKeywords: 'user',
       currentPage: 2,
     }
+
+    expect(result).toEqual(expected)
+  })
+
+  test('should convert "id" property to "id" with a string type', () => {
+    const user = {
+      email: 'email@email.com',
+      phone: '0123456789',
+      address: 'my home',
+      password: '1234',
+      isAdmin: false,
+      isActive: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+
+    const data: FetchUser[] = [
+      {
+        _id: 'John' as unknown as Types.ObjectId,
+        username: 'John',
+        ...user,
+      },
+      {
+        _id: 'Smith' as unknown as Types.ObjectId,
+        username: 'Smith',
+        ...user,
+      },
+    ]
+
+    const result = convertId(data)
+    const expected = [
+      {
+        id: 'John',
+        username: 'John',
+        ...user,
+      },
+      {
+        id: 'Smith',
+        username: 'Smith',
+        ...user,
+      },
+    ]
 
     expect(result).toEqual(expected)
   })
